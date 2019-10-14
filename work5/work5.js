540b69c00dc022326412ccd5a8431ed2ff751d51
apple <apple@jd505-70.local>
2019-10-14 11:45:28 +0800

    work5

--- work5/work5.js
+++ work5/work5.js
@@ -1,33 +1,30 @@
 str = '<table border="1">'
-        for(let i=9;i>0;--i){
-            str +='<tr>'
-            for(let i=9;i>0;--i){
-                str += '<tb>&nbsp;</tb>'
-            }
-            str +='</tr>'
-        }
-        str +='</table>'
-        document.activeElementID('table1').innerHTML = str
-
-
-        str = '<table border="1">'
-        for(let i=9;i>0;--i){
-            str +='<tr>'
-            for(var j=i;j>0;--j){
-                str += '<tb>&nbsp;</tb>'
-            }
-            str +='</tr>'
+for(let i=9;i>0;--i){
+    str += '<tr>'
+    for(var j=9;j>0;--j){
+        str +='<td>&nbsp;</td>'
+    }
+    str +='</tr>'
 }
 str += '</table>'
-document.getElementByID('table2').innerHTML = str
-str = '<table border="1>'
-for (let i=9;i>0;--i){
-    str +='</tr>'
+document.getElementById('table1').innerHTML = str
+str = '<table border="1">'
+for(let i=9;i>0;--i){
+    str += '<tr>'
     for(var j=i;j>0;--j){
-        str += '<tb>' + j + '*' +i + '=' + (j*i) + '</td>';
+        str +='<td>&nbsp;</td>'
+    }
+    str +='</tr>'
+}
+str += '</table>'
+document.getElementById('table2').innerHTML = str
+str = '<table border="1">'
+for(let i=9;i>0;--i){
+    str += '<tr>'
+    for(var j=i;j>0;--j){
+        str +='<td>' + j + '*' + i + '=' + (j*i) + '</td>';
     }
     str +='</tr>'
 }
 str += '</table>'
 document.getElementById('table3').innerHTML = str
-
