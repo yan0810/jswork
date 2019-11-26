30ad85a2e5b060a28c19d664e3d24847862fac15
yan0810 <1319305107@qq.com>
2019-11-26 19:46:54 +0800

    work13

--- work12-1/index1.js
+++ work12-1/index1.js
@@ -4,3 +4,8 @@ document.getElementById("cal").innerHTML = calendar(year)
 function setCalendar(year) {
     
 }
+let year = new Date().getFullYear()
+document.getElementById("cal").innerHTML = calendar(year)
+
+function setCalendar(year){
+}
