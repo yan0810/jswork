629dcdd0915eea92ffa23b5ad4eaaed8ec669ad3
yan0810 <1319305107@qq.com>
2019-12-23 11:50:55 +0800

    work18

---
+++ work18/index.js
@@ -0,0 +1,23 @@
+function send() {
+    let sno = document.getElementById('sno').value
+    let name = document.getElementById('name').value
+    let content = doc .getElementById('content').value
+    let xmlhttp;
+    if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xmlhttp.open('post', 'http://localhost:8080/ajax', true)
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.send('sno=' + sno + '&name=' + name +'&content=' + content)
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            let ajaxData = JSON.parse(xmlhttp.responseText)
+            resylt = ajaxData.reverse().map((val)=>{return JSON.stringify(val)})
+            document.getElementById("result").innerHTML = result.join('<br>');
+        }
+    }
+
+}
